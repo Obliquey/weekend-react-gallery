dcbf623c1bce342f53cb9132d29600f379760be3
Anders Boyum <Acboyum@gmail.com>
2023-04-29 10:50:35 -0500

    got db setup, and client-server-db interactions working. Now my photos are being stored and pulled from the database! Next to create a photo submission form 🥳

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,24 +1,43 @@
 const express = require('express');
 const router = express.Router();
-const galleryItems = require('../modules/gallery.data');
+// const galleryItems = require('../modules/gallery.data');
+const pool = require('../modules/pool');
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
+// "TRY ME" ~Anders
 
 // PUT Route
 router.put('/like/:id', (req, res) => {
-    console.log(req.params);
     const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
+    console.log("this is the gallery id to update:", galleryId)
+    
+    const sqlText = `
+        UPDATE gallery
+            SET likes = (likes + 1)
+            WHERE id = $1;
+    `;
+
+    pool.query(sqlText, [galleryId])
+        .then((dbRes) => {
+            console.log(`Successfully updated likes of ${galleryId}`, dbRes.rows);
+            res.send(200);
+        }).catch((dbErr) => {
+            console.log("Error updating likes in DB", dbErr);
+            res.sendStatus(500);
+        })
 }); // END PUT Route
 
 // GET Route
 router.get('/', (req, res) => {
-    res.send(galleryItems);
+
+
+    pool.query(`SELECT * FROM "gallery" ORDER BY "path" ASC;`)
+        .then((dbRes) => {
+            res.send(dbRes.rows)
+        }).catch((dbErr) => {
+            console.log("Error connecting to db,", dbErr);
+            res.sendStatus(500);
+        })
 }); // END GET Route
 
 module.exports = router;
