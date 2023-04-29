1bc3d89d9f384b9f9356af2ffb33283748beae5c
Anders Boyum <Acboyum@gmail.com>
2023-04-28 21:31:37 -0500

    Fixed wonky git commit history to get my like button, description reveal functionality back. Also added more photos

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,8 +1,22 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 2, path: 'images/twix_break.jpg', description: 'Photo of a Twix bar', likes: 0 },
-    { id: 3, path: 'images/capadocia.jpg', description: 'Photo of hot air balloons rising above the Capadocia region in Turkey', likes: 0 },
-    { id: 4, path: 'images/wedding_photo.jpg', description: 'Photo of my wife and I on our wedding day', likes: 0 }
+    { id: 1, path: 'images/wedding_photo.jpg', description: 'My wife and I on our wedding day', likes: 0 },
+    { id: 2, path: 'images/AnElephantEar.jpg', description: 'I love elephant ears. Or really, junk pastry foods in general', likes: 0 },
+    { id: 3, path: 'images/BeautifulWatersOfLakeMichigan.jpg', description: 'The tranquil and beautiful waters of Lake Michigan, seen from Mackinaw Island', likes: 0 },
+    { id: 4, path: 'images/CostaRicaSpaPond.jpg', description: 'The spa at the Costa Rican resort my wife and I went to on our honeymoon', likes: 0 },
+    { id: 5, path: 'images/FirstSummerMarriedCamperLiving.jpg', description: 'The camper my wife and I lived in the first summer we were married', likes: 0 },
+    { id: 6, path: 'images/HoneymoonBedroomCostaRica.jpg', description: 'The view from our Costa Rican bedroom. Hope you like waterfall white-noise to fall asleep too!', likes: 0 },
+    { id: 7, path: 'images/Koda&BrooklynAtJayCooke.jpg', description: 'My wife and dog at Jay Cooke Park', likes: 0 },
+    { id: 8, path: 'images/KodaAtJayCooke.jpg', description: 'Koda knows she looks great in the wild', likes: 0 },
+    { id: 9, path: 'images/LookingOverEthiopianLandscape.jpg', description: 'Checking out the Ethiopian mountain view', likes: 0 },
+    { id: 10, path: 'images/MyGoodFriendsBeingGoons.jpg', description: "My friends are goons, but they're my goons", likes: 0 },
+    { id: 11, path: 'images/MyTeamInEthiopia.jpg', description: 'The incredible people I lived and worked with in Ethiopia', likes: 0 },
+    { id: 12, path: 'images/OneTimeIMetJoshDun.jpg', description: 'Terrible picture, but one time I met Josh Dun. That was pretty cool', likes: 0 },
+    { id: 13, path: 'images/SunriseWithBalloonsOverCapadocia.jpg', description: 'Watching the hot air balloons rise over the capadocian landscape is something else', likes: 0 },
+    { id: 14, path: 'images/TheLibraryAtEphesus.jpg', description: 'Ephesus was very cool. This is the library', likes: 0 },
+    { id: 15, path: 'images/TheOnlyTimeIHadAProperVikingBeard.jpg', description: 'My Viking heritage sadly ends right before the beard and muscle genes', likes: 0 },
+    { id: 16, path: 'images/Velma&Shaggy.jpg', description: 'I never realized how much I look like Shaggy until I got second place in a Halloween costume contest', likes: 0 },
+    { id: 17, path: 'images/WatchingHotAirBalloonsOverCapadocia.jpg', description: 'Turkey is amazing, and I will definitely visit again', likes: 0 },
+    { id: 18, path: 'images/WhenWeFirstMetOurDog.jpg', description: 'One of the first times we met Koda, our dog-to-be', likes: 0 },
     
 ];
 
